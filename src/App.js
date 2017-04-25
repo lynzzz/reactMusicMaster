1c46fbb328d93f90001347056049e77e640dbe24
Zeng <uwhfz3@net.plm.eds.com>
2017-04-25 11:53:56 -0500

    added spotify API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./App.css"
 import {FormGroup, InputGroup, FormControl, Glyphicon} from 'react-bootstrap';
 
 class App extends Component{
+    
+	constructor(props){
+	    super(props);
+		this.state = {
+		    query : ''
+			
+		}
+	}
+	
+	search(){
+	    console.log(this.state.query);
+		const BASE_URL = 'https://api.spotify.com/v1/search?'
+	    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+		console.log(FETCH_URL);
+	}
 
     render(){
         return(
@@ -15,8 +30,17 @@ class App extends Component{
                        <FormControl
                            type="text"
                            placeholder="search an artiest"
+						   onChange={event => this.setState({query : event.target.value})}
+						   onKeyPress={
+						       event =>{
+						           if( event.key === "Enter"){
+								       this.search();
+								   }
+						       }
+						   
+						   }
                         />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={()=>this.search()}>
                            <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                    </InputGroup>
